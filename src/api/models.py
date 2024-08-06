901ca4dd52a6be563742225e59d12de3270f19ee
Cristian2499 <162387827+Cristian2499@users.noreply.github.com>
2024-08-06 03:37:52 +0000

    add music gender and music role

--- src/api/models.py
+++ src/api/models.py
@@ -8,7 +8,7 @@ class Gender(PyEnum):
     MALE = "Male"
     OTHER ="Other"
 
-class MusicGender(PyEnum):
+class MusicGenderEnum(PyEnum):
     ROCK = "Rock"
     JAZZ = "Jazz"
     URBAN = "Urban"
@@ -45,6 +45,7 @@ class Country(PyEnum):
     ARGENTINABSAS = "Argentina.BSAS"
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(180), unique=False, nullable=False)
@@ -55,8 +56,8 @@ class User(db.Model):
     nickname = db.Column(db.String(120), unique=True, nullable=False) #revisar el unique
     #date_of_birth = db.Colum(db.String(120), unique=False, nullable=False) #revisar
     gender = db.Column(db.Enum(Gender), unique=False, nullable=False)
-    #music_gender = db.Column(db.Enum(MusicGender), unique=False, nullable=False)
-    #role = db.Column(db.Enum(Role), unique=False, nullable=False)
+    music_genders = db.relationship("UserMusicGender", backref="user")
+    music_roles = db.relationship("UserMusicRole", backref="user")
     country = db.Column(db.Enum(Country), unique=False, nullable=False)
 
     def __repr__(self):
@@ -71,9 +72,70 @@ class User(db.Model):
             "lastname": self.lastname,
             "nickname": self.nickname,
             "gender": self.gender.value,
+            "music_genders": [music_gender.serialize() for music_gender in self.music_genders], 
+            "music_roles": [music_role.serialize() for music_role in self.music_roles],
             "country": self.country.value
         }
     
 
 class MusicGender(db.Model):
-    pass
+    __tablename__ = "music_gender"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    music_genders = db.relationship("UserMusicGender", backref="music_gender")
+
+    def __repr__(self):
+        return f'<MusicGender name: {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id, 
+            "name": self.name
+        }  
+
+class UserMusicGender(db.Model):
+    __tablename__ = "user_music_gender"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    music_gender_id = db.Column(db.Integer, db.ForeignKey("music_gender.id"))
+
+    def __repr__(self):
+        return f'<UserMusicGender user: {self.user_id} music gender: {self.music_gender_id}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id, 
+            "user_id": self.user_id, 
+            "music_gender_id": self.music_gender_id
+        }
+    
+class MusicRole(db.Model):
+    __tablename__ = "music_role"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    music_roles = db.relationship("UserMusicRole", backref="music_role")
+
+    def __repr__(self):
+        return f'<MusicRole name: {self.name}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id, 
+            "name": self.name
+        }
+
+class UserMusicRole(db.Model):
+    __tablename__ = "user_music_role"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    music_role_id = db.Column(db.Integer, db.ForeignKey("music_role.id"))
+
+    def __repr__(self):
+        return f'<UserMusicRole user: {self.user_id} music role: {self.music_role_id}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id, 
+            "user_id": self.user_id, 
+            "music_role_id": self.music_role_id
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from api.models import db, User, Gender, Role, Country
+from api.models import db, User, Gender, Role, Country, MusicGender, UserMusicGender
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
@@ -24,18 +24,17 @@ def regsiter_new_user():
         lastname = body.get("lastname", None)
         nickname = body.get("nickname", None)
         gender = body.get("gender", None)
-        role = body.get("role", None)
         country = body.get("country", None)
-        if email is None or password is None or name is None or lastname is None or nickname is None or gender is None or role is None or country is None:
+        if email is None or password is None or name is None or lastname is None or nickname is None or gender is None or country is None:
             return jsonify({"error": "all the fields must be completed"}), 400
         
-        email_is_taken = User.query.filter_by(email=email).first()
+        email_is_taken = User.query.filter_by(email=email).first() 
         if email_is_taken:
             return jsonify({"error": "Email already exist"}), 400
         
         password_hash = generate_password_hash(password)
         print(password_hash, len(password_hash))
-        user = User(email=email, password=password_hash, name=name, lastname=lastname, nickname=nickname,gender=Gender(gender), role=Role(role), country=Country(country), is_active=True )
+        user = User(email=email, password=password_hash, name=name, lastname=lastname, nickname=nickname,gender=Gender(gender), country=Country(country), is_active=True )
         db.session.add(user)
         db.session.commit()
         return jsonify({"msg": f"{nickname} created!"}), 201
@@ -44,6 +43,34 @@ def regsiter_new_user():
     except Exception as error:
         return jsonify({"error": f"{error}"}), 500
     
+
+@api.route("/musicgender", methods=["POST"])
+def create_music_gender():
+    try:
+        body = request.json
+        name = body.get("name", None)
+        if name is None:
+            return jsonify({"error": "name is required"}), 400
+
+    except Exception as error:
+        return jsonify({"error": f"{error}"}), 500
+
+@api.route("/user/musicgender/<int:music_gender_id>", methods=["POST"])
+@jwt_required()
+def create_user_music_gender(music_gender_id):
+    try: 
+        music_gender_is_taken = UserMusicGender.query.filter_by(user_id = user_data["id"], music_gender_id = music_gender_id).first()
+        if music_gender_is_taken:
+            return jsonify({"error": "Music gender already exist"}), 400
+        
+        user_data = get_jwt_identity()
+        user_music_gender = UserMusicGender(user_id = user_data["id"], music_gender_id = music_gender_id)
+        db.session.add(user_music_gender)
+        db.session.commit()
+        return jsonify({"msg": f"{music_gender_id} created!"}), 201
+
+    except Exception as error:
+        return jsonify({"error": f"{error}"}),500
 
 @api.route("/me", methods=["GET"])
 @jwt_required()
