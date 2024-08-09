18b2d5fd6e2212323e332c137cc7d72d7c755beb
Marvin Sojo <marvin526@gmail.com>
2024-08-09 19:28:55 -0400

    FIX: styles for card new artist

--- src/front/js/component/CardNewArtist.jsx
+++ src/front/js/component/CardNewArtist.jsx
@@ -4,19 +4,25 @@ import imgPlay from "../../img/img-new-artist/img-play.png";
 
 const CardNewArtist = ({ name, imageNewArtist }) => {
   return (
-    
-      <div className="bg-card card m-1 py-1 px-4 border-0">
-        <div className="text-white fs-5 mb-1">{name}</div>
-        <div className="d-flex">
-          <img className="img-new-artist" src={imageNewArtist} alt="" />
-          <div className="base-play bg-light d-flex justify-content-center align-items-center"><button className="btn-play"><i className="fa-solid fa-play fs-1"></i></button></div>
-        </div>
-        <div className="footer-card d-flex mt-2 text-white">
-          <button className="btn-like me-1"><i className="fa-regular fa-heart"></i></button>
-          <button><i className="fa-solid fa-user-plus"></i></button>
+    <div className="bg-card card m-1 py-1 px-4 border-0">
+      <div className="text-white fs-5 mb-1">{name}</div>
+      <div className="d-flex">
+        <img className="img-new-artist" src={imageNewArtist} alt="" />
+        <div className="base-play bg-light d-flex justify-content-center align-items-center">
+          <button className="btn-play">
+            <i className="fa-solid fa-play fs-1"></i>
+          </button>
         </div>
       </div>
-    
+      <div className="footer-card d-flex my-2 text-white">
+        <button className="btn-like me-1">
+          <i className="fa-regular fa-heart"></i>
+        </button>
+        <button className="btn-user-plus">
+          <i className="fa-solid fa-user-plus"></i>
+        </button>
+      </div>
+    </div>
   );
 };
 
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -9,6 +9,10 @@ import CardNewArtist from "../component/CardNewArtist.jsx";
 //test images for cards
 import img01 from "../../img/img-new-artist/img-01.png";
 import img02 from "../../img/img-new-artist/img-02.png";
+import img03 from "../../img/img-new-artist/img-03.png";
+import img04 from "../../img/img-new-artist/img-04.png";
+import img05 from "../../img/img-new-artist/img-05.png";
+import img06 from "../../img/img-new-artist/img-06.png";
 
 
 
@@ -19,8 +23,14 @@ export const Home = () => {
 		<div className="animate__animated animate__fadeIn">
 		<Navbar />
 		<Jumbotron/>
-		<div className="d-flex justify-content-center align-items-center">
-		<CardNewArtist name="John Doe - Guitarist" imageNewArtist={img01} footer="Soy el footer" />
+		<div className="container-fluid d-flex row justify-content-center align-items-center m-0">
+		<div className="container d-flex justify-content-center my-2 py-2"><h2>Discover New Artists</h2></div>
+		<CardNewArtist name="John Doe - Guitarist" imageNewArtist={img01}/>
+		<CardNewArtist name="Jane Smith - Singer" imageNewArtist={img02}/>
+		<CardNewArtist name="Mike Johnson - M. Producer" imageNewArtist={img03}/>
+		<CardNewArtist name="Emily Davis - Guitarist" imageNewArtist={img04}/>
+		<CardNewArtist name="Chris Lee - Cellist" imageNewArtist={img05}/>
+		<CardNewArtist name="Anna Brown - M. Producer" imageNewArtist={img06}/>
 		</div>
 		<Footer />
 		</div>
