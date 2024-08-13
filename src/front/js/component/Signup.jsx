77ab842463ab564b49b673f6bf307eef560521fc
Ursula Jardon <ursynna.jardon@gmail.com>
2024-08-12 22:30:19 -0600

    signup part1

--- src/front/js/component/Signup.jsx
+++ src/front/js/component/Signup.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import bgImage from "../../img/morenito.png";
 import "../../styles/signup.css";
+import { Context } from "../store/appContext";
+
 
 const Signup = () => {
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [gender, setGender] = useState("");
+  const [country, setCountry] = useState("");
+  const handleSubmitRegister = (e) => {
+    e.preventDefault();
+    console.log(email, password, name, lastname, nickname, gender, country);
+  }
+  const handleChange = (event) => {
+    setGender(event.target.value);
+  };
+  const handleChangeCountry = (event) => {
+    setCountry(event.target.value);
+  };
+
+
   return (
     <>
       <div className="body" style={{ backgroundImage: `url(${bgImage})` }}>
@@ -10,15 +32,87 @@ const Signup = () => {
         <h2>Create Your Account</h2>
         <div className="form-container">
           <div className="form-content">
-            <form>
-              <input type="text" placeholder="Nombre" required />
-              <input type="text" placeholder="Apellido" required />
-              <input type="email" placeholder="Email address" required />
-              <input type="text" placeholder="Nickname" required />
-              <input type="text" placeholder="Género" required />
-              <input type="text" placeholder="País" required />
-              <input type="text" placeholder="Género Musical" required />
-              <input type="text" placeholder="Rol Musical" required />
+            <form onSubmit={handleSubmitRegister}>
+              <input
+                type="text"
+                placeholder="Name"
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Lastname"
+                required
+                value={lastname}
+                onChange={(e) => setLastname(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Nickname"
+                required
+                value={nickname}
+                onChange={(e) => setNickname(e.target.value)}
+              />
+              <input
+                type="email"
+                placeholder="Email address"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {/* <input
+                type="text"
+                placeholder="Gender"
+                required
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+              /> */}
+              {/* <input
+                type="text"
+                placeholder="Country"
+                required
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+              /> */}
+              <select
+                id="gender-select"
+                className="form-select"
+                value={gender}
+                onChange={handleChange}
+                aria-label="Default select example"
+              >
+                <option value="" disabled>Select your gender</option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="others">Others</option>
+              </select>
+              <select
+                id="gender-select"
+                className="form-select"
+                value={country}
+                onChange={handleChangeCountry}
+                aria-label="Default select example"
+              >
+                <option value="" disabled>Select your country</option>
+                <option value="Colombia-Bogota">Colombia-Bogota</option>
+                <option value="Venezuela-Caracas">Venezuela-Caracas</option>
+                <option value="Ecuador-Quito">Ecuador-Quito</option>
+                <option value="Peru-Lima">Peru-Lima</option>
+                <option value="Bolivia-La paz">Bolivia-La paz</option>
+                <option value="Mexico-CDMX">Mexico-CDMX</option>
+                <option value="Chile-Santiago">Chile-Santiago</option>
+                <option value="Paraguay-Asuncion">Paraguay-Asuncion</option>
+                <option value="Uruguay-Montevideo"> Uruguay-Montevideo</option>
+                <option value="Argentina-BSAS">Argentina-BSAS</option>
+              </select>
               <button type="submit">Registrarse</button>
             </form>
             <p>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -16,37 +16,28 @@ const getState = ({ getStore, getActions, setStore }) => {
 			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
+			register: async (email,password,name,lastname,nickname,gender,country) => {
+				try {
+					const response = await fetch(
+						process.env.BACKEND_URL + "/api/registrer",
+						{
+							method: "POST",
+							headers: { 
+								"Content-Type": "application/json",
+							},
+							body: JSON.stringify({email,password,name,lastname,nickname,gender,country}),
+						}	
+					);
+					if (!response.ok) {
+						return false;
+					}
+					const data = response.json();
 					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
+				} catch (error) {
+					console.log(error);
 				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			},		
+	
 		}
 	};
 };
