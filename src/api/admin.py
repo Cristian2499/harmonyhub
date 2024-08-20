1b896fa25e99d38d8effe3498cae2238e9fbaf1e
Marvin Sojo <marvin526@gmail.com>
2024-08-19 22:13:04 -0400

    ADD: connect users

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, MusicGender, UserMusicGender, MusicRole, UserMusicRole, Song, TrackLikes
+from .models import db, User, MusicGender, UserMusicGender, MusicRole, UserMusicRole, Song, TrackLikes, ConnectUsers
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -18,6 +18,7 @@ def setup_admin(app):
     admin.add_view(ModelView(UserMusicRole, db.session))
     admin.add_view(ModelView(Song, db.session))
     admin.add_view(ModelView(TrackLikes, db.session))
+    admin.add_view(ModelView(ConnectUsers, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
